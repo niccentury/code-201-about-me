09706ec0e9b5e03c9c06d88d2225b66cfd843080
Danul De Leon <danul.deleon@gmail.com>
2018-07-31 14:10:19 -0700

    All questions working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 "use strict";
 
 console.log('js is linked');
+var isTrue;
 
-//commence game
+//Commence game
 alert('Hey there!  Let\'s play a little game.');
 
 //First Question
@@ -10,7 +11,7 @@ var q1 = prompt('Do I know how to speak Spanish?');
 console.log('The answer to question one is: '+q1)
 
 //Check First Question
-var isTrue = check(q1);
+isTrue = check(q1);
 console.log('**This is the return from check function for q1: '+isTrue);
 
 //First Response
@@ -20,26 +21,65 @@ if(isTrue == 'Yes'){
     alert('NOPE!');
 }
 
-// var q2 = prompt('Did I originally live in Texas?');
-// console.log('The answer to question one is: '+q2)
+//Second Question
+var q2 = prompt('Did I originally live in Texas?');
+console.log('The answer to question one is: '+q2)
 
-// var q3 = prompt('Is my favorite food fish sticks?');
-// console.log('The answer to question one is: '+q3)
+//Check Second Question
+isTrue = check(q2);
+console.log('**This is the return from check function for q1: '+isTrue);
 
-// var q4 = prompt('Do I drink a questionable amount of coffee?');
-// console.log('The answer to question one is: '+q4)
+//Second Response
+if(isTrue == 'Yes'){
+    alert('You are correct!');
+}else{
+    alert('NOPE!');
+}
 
-// var q5 = prompt('Can you get me to do anything by bribing me with snacks?');
-// console.log('The answer to question one is: '+q5)
+//Third Question
+var q3 = prompt('Is my favorite food fish sticks?');
+console.log('The answer to question one is: '+q3);
 
-// var questions = [q1, q2, q3, q4, q5];
-// console.log(questions);
+//Check Third Question
+isTrue = check(q3);
+console.log('**This is the return from check function for q1: '+isTrue);
 
-// for( var i = 0; i < questions.length; i++){
-//     console.log(questions[i]);
-//     //if(questions[i].toUpperCase() == 'YES' || 'Y'){
-//     //    q1 = 'Yes'
-//     }
+//Third Response
+if(isTrue == 'No'){
+    alert('You are correct!');
+}else{
+    alert('NOPE!');
+}
+
+//Fourth Question
+var q4 = prompt('Do I drink a questionable amount of coffee?');
+console.log('The answer to question one is: '+q4);
+
+//Check Fourth Question
+isTrue = check(q4);
+console.log('**This is the return from check function for q1: '+isTrue);
+
+//Fourth Response
+if(isTrue == 'Yes'){
+    alert('You are correct!');
+}else{
+    alert('NOPE!');
+}
+
+//Fifth Question
+var q5 = prompt('Can you get me to do anything by bribing me with snacks?');
+console.log('The answer to question one is: '+q5)
+
+//Check Fifth Question
+isTrue = check(q5);
+console.log('**This is the return from check function for q1: '+isTrue);
+
+//Fifth Response
+if(isTrue == 'Yes'){
+    alert('You are correct!');
+}else{
+    alert('NOPE!');
+}
 
 function check(answer){
     console.log('This is the answer within the function: '+answer);
