1db632cb83455f2db5db510287d35a74c789bdc3
Danul De Leon <danul.deleon@gmail.com>
2018-07-31 14:30:38 -0700

    added function to compare answers

--- js/app.js
+++ js/app.js
@@ -15,11 +15,7 @@ isTrue = check(q1);
 console.log('**This is the return from check function for q1: '+isTrue);
 
 //First Response
-if(isTrue == 'Yes'){
-    alert('You are correct!');
-}else{
-    alert('NOPE!');
-}
+isRight(isTrue, 1);
 
 //Second Question
 var q2 = prompt('Did I originally live in Texas?');
@@ -30,11 +26,7 @@ isTrue = check(q2);
 console.log('**This is the return from check function for q1: '+isTrue);
 
 //Second Response
-if(isTrue == 'Yes'){
-    alert('You are correct!');
-}else{
-    alert('NOPE!');
-}
+isRight(isTrue, 2);
 
 //Third Question
 var q3 = prompt('Is my favorite food fish sticks?');
@@ -45,11 +37,7 @@ isTrue = check(q3);
 console.log('**This is the return from check function for q1: '+isTrue);
 
 //Third Response
-if(isTrue == 'No'){
-    alert('You are correct!');
-}else{
-    alert('NOPE!');
-}
+isRight(isTrue, 3);
 
 //Fourth Question
 var q4 = prompt('Do I drink a questionable amount of coffee?');
@@ -60,11 +48,7 @@ isTrue = check(q4);
 console.log('**This is the return from check function for q1: '+isTrue);
 
 //Fourth Response
-if(isTrue == 'Yes'){
-    alert('You are correct!');
-}else{
-    alert('NOPE!');
-}
+isRight(isTrue, 4);
 
 //Fifth Question
 var q5 = prompt('Can you get me to do anything by bribing me with snacks?');
@@ -75,14 +59,12 @@ isTrue = check(q5);
 console.log('**This is the return from check function for q1: '+isTrue);
 
 //Fifth Response
-if(isTrue == 'Yes'){
-    alert('You are correct!');
-}else{
-    alert('NOPE!');
-}
+isRight(isTrue, 5);
 
+
+//Function used to check Capitalization of answers and standardize.
 function check(answer){
-    console.log('This is the answer within the function: '+answer);
+    console.log('This is the answer within the check function: '+answer);
     var newAnswer = answer.toUpperCase();
     if(newAnswer == 'YES' || newAnswer == 'Y'){
         return 'Yes';
@@ -90,5 +72,16 @@ function check(answer){
         return 'No';
     }else{ 
         return null; 
+    }
+}
+
+//Function used to compare answer if correct or not.
+function isRight(answer, question){
+    var compare = ['Yes', 'Yes', 'No', 'Yes', 'Yes'];
+    console.log('This is the answer within the isRight function: '+answer);
+    if(answer === compare[question-1]){
+        alert('You are correct!');
+    }else{
+        alert('NOPE!');
     }
 }
