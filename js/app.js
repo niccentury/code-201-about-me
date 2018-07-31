ee213efd4a0f52cd00fe015c553fcef45f08bda5
Danul De Leon <danul.deleon@gmail.com>
2018-07-31 15:28:39 -0700

    added basic CSS and a null response

--- js/app.js
+++ js/app.js
@@ -41,7 +41,9 @@ function check(answer){
 //Function used to compare answer if correct or not.
 function isRight(answer, question, compare){
     console.log('This is the answer within the isRight function: '+answer);
-    if(answer === compare[question-1]){
+    if(answer == null){
+        alert('That wasn\'t a proper answer.  Maybe check your spelling!');
+    }else if(answer === compare[question-1]){
         alert('You are correct!');
         return;
     }else{
@@ -49,58 +51,3 @@ function isRight(answer, question, compare){
         return;
     }
 }
-
-// //First Question
-// // var q1 = prompt('Do I know how to speak Spanish?');
-// console.log('The answer to question one is: '+q1)
-
-// //Check First Question
-// isTrue = check(q1);
-// console.log('**This is the return from check function for q1: '+isTrue);
-
-// //First Response
-// isRight(isTrue, 1, compare);
-
-// //Second Question
-// // var q2 = prompt('Did I originally live in Texas?');
-// console.log('The answer to question one is: '+q2)
-
-// //Check Second Question
-// isTrue = check(q2);
-// console.log('**This is the return from check function for q1: '+isTrue);
-
-// //Second Response
-// isRight(isTrue, 2, compare);
-
-// //Third Question
-// // var q3 = prompt('Is my favorite food fish sticks?');
-// console.log('The answer to question one is: '+q3);
-
-// //Check Third Question
-// isTrue = check(q3);
-// console.log('**This is the return from check function for q1: '+isTrue);
-
-// //Third Response
-// isRight(isTrue, 3, compare);
-
-// //Fourth Question
-// // var q4 = prompt('Do I drink a questionable amount of coffee?');
-// console.log('The answer to question one is: '+q4);
-
-// //Check Fourth Question
-// isTrue = check(q4);
-// console.log('**This is the return from check function for q1: '+isTrue);
-
-// //Fourth Response
-// isRight(isTrue, 4, compare);
-
-// //Fifth Question
-// // var q5 = prompt('Can you get me to do anything by bribing me with snacks?');
-// console.log('The answer to question one is: '+q5)
-
-// //Check Fifth Question
-// isTrue = check(q5);
-// console.log('**This is the return from check function for q1: '+isTrue);
-
-// //Fifth Response
-// isRight(isTrue, 5, compare);
