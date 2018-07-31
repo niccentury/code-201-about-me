fed54ec370d98050c68a1c37c0948b61c75d6027
Danul De Leon <danul.deleon@gmail.com>
2018-07-31 13:15:04 -0700

    Added prompts for user input

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
+"use strict";
+
 console.log('js is linked');
-alert('Hey there!');
+alert('Hey there!  Let\'s play a little game.');
+
+var q1 = prompt('Do I know how to speak Spanish?');
+console.log('The answer to question one is: '+q1)
+
+var q2 = prompt('Did I originally live in Texas?');
+console.log('The answer to question one is: '+q2)
+
+var q3 = prompt('Is my favorite food fish sticks?');
+console.log('The answer to question one is: '+q3)
+
+var q4 = prompt('Do I drink a questionable amount of coffee?');
+console.log('The answer to question one is: '+q4)
+
+var q5 = prompt('Can you get me to do anything by bribing me with snacks?');
+console.log('The answer to question one is: '+q5)
