c81a2281ff5addf5c61efaaee3964d78f6393b53
Danul De Leon <danul.deleon@gmail.com>
2018-08-01 14:06:27 -0700

    Added the 6th question

--- js/app.js
+++ js/app.js
@@ -1,69 +1,70 @@
 "use strict";
 
 console.log('js is linked');
-var compare = [1, 1, 0, 1, 1];
-// var score = 0;
+var score = 0;
 var answer = [];
+var randNum = Math.floor((Math.random() * 10) + 1);
+var compare = [1, 1, 0, 1, 1, randNum];
 var questions = [
   'Do I know how to speak Spanish?',
   'Did I originally live in Texas?',
   'Is my favorite food fish sticks?',
   'Do I drink a questionable amount of coffee?',
-  'Can you get me to do anything by bribing me with snacks?'
+  'Can you get me to do anything by bribing me with snacks?',
+  'Guess a number between 1 and 10'
 ];
 
 
 //Commence game
 var userName = prompt('Please enter your name: ');
-console.log('The user name is: '+userName)
+console.log('The user name is: '+userName);
 alert('Hey there, '+ userName +'!  Let\'s play a little game.');
 
 //Runs through each question
 for(var i=0; i<questions.length; i++){
-  answer[i]= prompt(questions[i]).toUpperCase();
-  var newAnswer = answer[i];
+  var newAnswer= prompt(questions[i]).toUpperCase();
+  answer[i] = newAnswer;
   console.log('The user input is: '+answer[i]);
-  if(newAnswer === 'YES' || newAnswer === 'Y'){
-    if(compare[i] === 1){
-      alert('You are correct!');
+  if(i < 5){
+    //Checks if answer matches yes
+    if(newAnswer === 'YES' || newAnswer === 'Y'){
+      if(compare[i] === 1){
+        alert('You are correct!');
+        score++;
+        console.log('The current score is: ' +score);
+      }else{
+        alert('Nope!');
+        console.log('The current score is: ' +score);
+      }
+    }else if(newAnswer === 'NO' || newAnswer === 'N'){ //Checks is answer matches no
+      if(compare[i] === 0){
+        alert('You are correct!');
+        score++;
+        console.log('The current score is: ' +score);
+      }else{
+        alert('Nope!');
+        console.log('The current score is: ' +score);
+      }
+    }else{  //Responds if answer is not yes or no
+      alert('That wasn\'t a proper answer.  Maybe check your spelling!');
+    }   
+  }
+  else{
+    console.log('The random number is: '+compare[i]);
+    if(isNaN(newAnswer)){
+      alert('That wasn\'t even a number! Get with it, '+userName+'!');
     }else{
-      alert('Nope!');
+      if(newAnswer === compare[i]){
+        alert('You are correct!');
+        score++;
+        console.log('The current score is: ' +score);
+      }else{
+        alert('Nope!');
+        console.log('The current score is: ' +score);
+      }
     }
-  }else if(newAnswer === 'NO' || newAnswer === 'N'){
-    if(compare[i] === 0){
-      alert('You are correct!');
-    }else{
-      alert('Nope!');
-    }
-  }else{
-    alert('That wasn\'t a proper answer.  Maybe check your spelling!');
-  }   
+  }
 }
+//Displays the answer array
 console.log(answer);
-
-// //Function used to check Capitalization of answers and standardize.
-// function check(answer){
-//     console.log('This is the answer within the check function: '+answer);
-//     var newAnswer = answer.toUpperCase();
-//     if(newAnswer == 'YES' || newAnswer == 'Y'){
-//         return 1;
-//     }else if(newAnswer == 'NO' || newAnswer == 'N'){
-//         return 0;
-//     }else{
-//         return null;
-//     }
-// }
-
-// //Function used to compare answer if correct or not.
-// function isRight(answer, question, compare){
-//     console.log('This is the answer within the isRight function: '+answer);
-//     if(answer == null){
-//         alert('That wasn\'t a proper answer.  Maybe check your spelling!');
-//     }else if(answer === compare[question-1]){
-//         alert('You are correct!');
-//         return;
-//     }else{
-//         alert('NOPE!');
-//         return;
-//     }
-// }
+alert('You got '+score+' out of 6 correct, '+userName+'!');
