e52b49e27b9aa0b517ad66096abdcaa432fb5942
Danul De Leon <danul.deleon@gmail.com>
2018-08-01 13:40:00 -0700

    made code without functions

--- js/app.js
+++ js/app.js
@@ -1,54 +1,69 @@
 "use strict";
 
 console.log('js is linked');
-var isTrue;
 var compare = [1, 1, 0, 1, 1];
-var score = 0;
+// var score = 0;
+var answer = [];
 var questions = [
-    'Do I know how to speak Spanish?',
-    'Did I originally live in Texas?',
-    'Is my favorite food fish sticks?',
-    'Do I drink a questionable amount of coffee?',
-    'Can you get me to do anything by bribing me with snacks?'
+  'Do I know how to speak Spanish?',
+  'Did I originally live in Texas?',
+  'Is my favorite food fish sticks?',
+  'Do I drink a questionable amount of coffee?',
+  'Can you get me to do anything by bribing me with snacks?'
 ];
 
 
 //Commence game
 var userName = prompt('Please enter your name: ');
+console.log('The user name is: '+userName)
 alert('Hey there, '+ userName +'!  Let\'s play a little game.');
 
 //Runs through each question
 for(var i=0; i<questions.length; i++){
-    var answer = prompt(questions[i]);
-    console.log('User input for question '+(i+1)+': '+answer);
-    isTrue = check(answer);
-    console.log('Return from Check function: '+isTrue);
-    isRight(isTrue, i+1, compare, score);
-}
-
-//Function used to check Capitalization of answers and standardize.
-function check(answer){
-    console.log('This is the answer within the check function: '+answer);
-    var newAnswer = answer.toUpperCase();
-    if(newAnswer == 'YES' || newAnswer == 'Y'){
-        return 1;
-    }else if(newAnswer == 'NO' || newAnswer == 'N'){
-        return 0;
+  answer[i]= prompt(questions[i]).toUpperCase();
+  var newAnswer = answer[i];
+  console.log('The user input is: '+answer[i]);
+  if(newAnswer === 'YES' || newAnswer === 'Y'){
+    if(compare[i] === 1){
+      alert('You are correct!');
     }else{
-        return null;
+      alert('Nope!');
     }
-}
-
-//Function used to compare answer if correct or not.
-function isRight(answer, question, compare){
-    console.log('This is the answer within the isRight function: '+answer);
-    if(answer == null){
-        alert('That wasn\'t a proper answer.  Maybe check your spelling!');
-    }else if(answer === compare[question-1]){
-        alert('You are correct!');
-        return;
+  }else if(newAnswer === 'NO' || newAnswer === 'N'){
+    if(compare[i] === 0){
+      alert('You are correct!');
     }else{
-        alert('NOPE!');
-        return;
+      alert('Nope!');
     }
+  }else{
+    alert('That wasn\'t a proper answer.  Maybe check your spelling!');
+  }   
 }
+console.log(answer);
+
+// //Function used to check Capitalization of answers and standardize.
+// function check(answer){
+//     console.log('This is the answer within the check function: '+answer);
+//     var newAnswer = answer.toUpperCase();
+//     if(newAnswer == 'YES' || newAnswer == 'Y'){
+//         return 1;
+//     }else if(newAnswer == 'NO' || newAnswer == 'N'){
+//         return 0;
+//     }else{
+//         return null;
+//     }
+// }
+
+// //Function used to compare answer if correct or not.
+// function isRight(answer, question, compare){
+//     console.log('This is the answer within the isRight function: '+answer);
+//     if(answer == null){
+//         alert('That wasn\'t a proper answer.  Maybe check your spelling!');
+//     }else if(answer === compare[question-1]){
+//         alert('You are correct!');
+//         return;
+//     }else{
+//         alert('NOPE!');
+//         return;
+//     }
+// }
