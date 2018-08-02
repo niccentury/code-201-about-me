d695283b90cba7b7cae5bad9cc1b40ee2a14139d
niccentury <cjdcec@gmail.com>
2018-08-02 14:40:35 -0700

    Added rest of Functions

--- js/app.js
+++ js/app.js
@@ -17,90 +17,100 @@ var questions = [
 
 var userName = getName();
 //Commence game
-function getName(){
+function getName() {
   var Name = prompt('What\'s your Name?');
   return Name;
 }
-console.log('The user name is: '+userName);
-alert('Hey there, '+ userName +'!  Let\'s play a little game.');
+console.log('The user name is: ' + userName);
+alert('Hey there, ' + userName + '!  Let\'s play a little game.');
 
 //Runs through each question
-for(var i=0; i<questions.length; i++){
-  var newAnswer= prompt(questions[i]).toUpperCase();
-  console.log('The user input is: '+answer[i]);
-  if(i < 5){
+for (var i = 0; i < questions.length; i++) {
+  var newAnswer = prompt(questions[i]).toUpperCase();
+  console.log('The user input is: ' + newAnswer);
+  if (i < 5) {
+    checkAnswer(newAnswer);
     //Checks if answer matches yes
-    if(newAnswer === 'YES' || newAnswer === 'Y'){
-      if(compare[i] === 1){
-        alert('You are correct!');
-        score++;
-        console.log('The current score is: ' +score);
-      }else{
-        alert('Nope!');
-        console.log('The current score is: ' +score);
-      }
-    }else if(newAnswer === 'NO' || newAnswer === 'N'){ //Checks is answer matches no
-      if(compare[i] === 0){
-        alert('You are correct!');
-        score++;
-        console.log('The current score is: ' +score);
-      }else{
-        alert('Nope!');
-        console.log('The current score is: ' +score);
-      }
-    }else{  //Responds if answer is not yes or no
-      alert('That wasn\'t a proper answer.  Maybe check your spelling!');
-    }
+
   }
-  //Sixth question checking for numbers
-  else if(i===5){
-    console.log('The random number is: '+compare[i]);
-    for(var x = 0; x<4; x++){ //allows for 4 tries
-      if(x>0 && x<4){
-        newAnswer= prompt(questions[i]).toUpperCase();
-      }
-      if(newAnswer == compare[i]){
-        alert('You are correct!');
-        score++;
-        console.log('The current score is: ' +score);
-        break;
-      }else if(isNaN(newAnswer)){
-        alert('That wasn\'t even a number! Get with it, '+userName+'! You have '+(3-x)+' tries left!');
-      }else{
-        alert('Nope! You have '+(3-x)+' tries left!');
-        console.log('The current score is: ' +score);
-      }
-    }
+
+  else if (i === 5) {
+    console.log('The random number is: ' + compare[i]);
+    checkNumber();
   }
-  else{
-    console.log('Cities in Texas: '+compare[i]);
-    for(var y = 0; y<6 ; y++){  // allows for 6 tries
-      if(y > 0){
-        newAnswer= prompt(questions[i]).toUpperCase();
-      }
-      for(var j=0; j<compare[i].length; j++){
-        if(newAnswer === compare[i][j]){
-          alert('You are correct!');
-          score++;
-          console.log('The current score is: ' +score);
-          var getOut = true;
-          break;
-        }else if(j===compare[i].length-1){
-          alert('Nope! Try Again! You have '+(5-y)+' tries left!');
-          console.log('The current score is: ' +score);
-          continue;
-        }
-      }
-      if(getOut){
-        break;
-      }
-    }
-  }
+  else {
+    console.log('Cities in Texas: ' + compare[i]);
+    checkCity();
+}
 }
 //Displays the answer array
-if(score<questions.length){
-  alert('You got '+score+' out of 7 correct, '+userName+'! You can do better than that!');
-}else{
-  alert('You got '+score+' out of 7 correct, '+userName+'! A perfect score!');
+if (score < questions.length) {
+  alert('You got ' + score + ' out of 7 correct, ' + userName + '! You can do better than that!');
+} else {
+  alert('You got ' + score + ' out of 7 correct, ' + userName + '! A perfect score!');
 }
 
+function checkAnswer(newAnswer) {
+  if (newAnswer === 'YES' || newAnswer === 'Y') {
+    if (compare[i] === 1) {
+      alert('You are correct!');
+      score++;
+      console.log('The current score is: ' + score);
+    } else {
+      alert('Nope!');
+      console.log('The current score is: ' + score);
+    }
+  } else if (newAnswer === 'NO' || newAnswer === 'N') { //Checks is answer matches no
+    if (compare[i] === 0) {
+      alert('You are correct!');
+      score++;
+      console.log('The current score is: ' + score);
+    } else {
+      alert('Nope!');
+      console.log('The current score is: ' + score);
+    }
+  } else {  //Responds if answer is not yes or no
+    alert('That wasn\'t a proper answer.  Maybe check your spelling!');
+  }
+}
+function checkNumber() {
+  for (var x = 0; x < 4; x++) { //allows for 4 tries}
+    if (x > 0 && x < 4) {
+      newAnswer = prompt(questions[i]).toUpperCase();
+    }
+    if (newAnswer == compare[i]) {
+      alert('You are correct!');
+      score++;
+      console.log('The current score is: ' + score);
+      break;
+    } else if (isNaN(newAnswer)) {
+      alert('That wasn\'t even a number! Get with it, ' + userName + '! You have ' + (3 - x) + ' tries left!');
+    } else {
+      alert('Nope! You have ' + (3 - x) + ' tries left!');
+      console.log('The current score is: ' + score);
+    }
+  }
+}
+function checkCity() {
+  for (var y = 0; y < 6; y++) {  // allows for 6 tries
+    if (y > 0) {
+      newAnswer = prompt(questions[i]).toUpperCase();
+    }
+    for (var j = 0; j < compare[i].length; j++) {
+      if (newAnswer === compare[i][j]) {
+        alert('You are correct!');
+        score++;
+        console.log('The current score is: ' + score);
+        var getOut = true;
+        break;
+      } else if (j === compare[i].length - 1) {
+        alert('Nope! Try Again! You have ' + (5 - y) + ' tries left!');
+        console.log('The current score is: ' + score);
+        continue;
+      }
+    }
+    if (getOut) {
+      break;
+    }
+  }
+}
